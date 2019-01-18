44a11420dbbe5dfd688469455e6e17b8695c1822
wassul <wa@ad.com>
2019-01-17 23:13:38 -0800

    in plot update

--- TestUi/ppf/ui/InPlotImage.py
+++ TestUi/ppf/ui/InPlotImage.py
@@ -3,14 +3,13 @@
 from PyQt5.QtCore import (Qt)
 #from PyQt5.QtWidgets import (QTreeView, QDockWidget)
 from PyQt5.QtGui import (QPainter, QColor, QFont)
-from PyQt5.Qt import QWidget
+from PyQt5.Qt import QWidget, QPen, QGraphicsScene
 from ppf.core.Utils import g
 
 class InPlotImage(QWidget):
     
     #self.mainGui
     #
-    
     def __init__(self, mainGui):
         super().__init__()
         self.mainGui = mainGui
@@ -26,8 +25,7 @@ class InPlotImage(QWidget):
     
     def mousePressEvent(self, event):
         g().outText.print( "mousePressEvent" );
-        g().outText.print(event)
-
+        g().outText.print('x=' + str(event.pos().x()) + 'y=' + str(event.pos().y()))
 
     def mouseReleaseEvent(self, event):
         self.text = 'Click'
@@ -37,6 +35,15 @@ class InPlotImage(QWidget):
         #    # Perform double click action.
         ##   #self.message = "Double Click"
         #    #self.update()
+        
+    
+    def mouseMoveEvent(self, event):
+        g().outText.print( "mouseMoveEvent" );
+        g().outText.print('x=' + str(event.pos().x()) + 'y=' + str(event.pos().y()))
+        
+    def mouseLeaveEvent(self, event):
+        g().outText.print( "mouseLeaveEvent" );
+        g().outText.print('x=' + str(event.pos().x()) + 'y=' + str(event.pos().y()))
  
     def mouseDoubleClickEvent(self, event):
         self.text = 'double click'
@@ -50,8 +57,17 @@ class InPlotImage(QWidget):
         qp.setFont(QFont('Decorative', 10))
         qp.drawText(event.rect(), Qt.AlignCenter, self.text) 
     
+
+'''
+class Scene(QGraphicsScene):
     
+    pens = [ QPen(Qt.green), QPen(Qt.green, 4) ]
     
+    def __init__(self, parent):
+        QGraphicsScene.__init__(self, parent)
+        self.setSceneRect(-100, -100, 200, 200)
         
+    
+'''        
         
         
